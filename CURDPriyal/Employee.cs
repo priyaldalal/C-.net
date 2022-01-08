0f4194b7864a82ad3fc59eba941ac02e789f3398
Priyal Dalal <84765716+priyaldalal@users.noreply.github.com>
2022-01-08 14:54:03 +0530

    Add files via upload

---
+++ CURDPriyal/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CURDPriyal
+{
+    public class Employee
+    {
+        public int EmpId;
+        public string FName;
+        public string LName;
+        public string MName;
+        public string Email;
+        public DateTime Dob;
+        public string MaritalStatus;
+        public string Hobbies;
+        public string HomeMobile;
+        public string OfficeMobile;
+        public string Address;
+        public string Pincode;
+        public string State;
+        public string Nationality;
+        public DateTime Doj;
+        public DateTime CreatedDateTime;
+        public DateTime ModifiedDateTime;  
+    }
+}
